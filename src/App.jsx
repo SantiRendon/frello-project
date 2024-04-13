893ebe1089c05252048a337e5bcc533f4eb9b1ae
HolaJose2 <miguelgarcia.m@hotmail.com>
2024-04-13 17:48:07 -0500

    handle update task position

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,42 @@ import { useState } from "react";
 function App() {
   const [data, setData] = useState(InitialData);
 
+  const handleDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) return;
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    
+    const column = data.columns[source.droppableId];
+    const newTaskIds = Array.from(column.tasksIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      tasksIds: newTaskIds,
+    };
+
+    const newData = {
+      ...data,
+      columns: {
+        ...data.columns,
+        [newColumn.id]: newColumn,
+      },
+    };
+
+    setData(newData);
+  };
+
   return (
     <>
       <div className="flex">
-        <DragDropContext>
+        <DragDropContext onDragEnd={handleDragEnd}>
           {data.orderColumns.map((columnId) => {
             const column = data.columns[columnId];
             const tasks = column.tasksIds.map((taskId) => data.tasks[taskId]);
